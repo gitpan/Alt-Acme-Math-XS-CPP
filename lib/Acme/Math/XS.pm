7818805b5ecf71bcccd68ebd4ab0228a03573206
Ingy döt Net <ingy@cpan.org>
2014-12-18 07:48:23 +0000

    Import of INGY/Alt-Acme-Math-XS-CPP-0.0.1.tar.gz from CPAN.
    gitpan-cpan-distribution: Alt-Acme-Math-XS-CPP
    gitpan-cpan-version:      0.0.1
    gitpan-cpan-path:         INGY/Alt-Acme-Math-XS-CPP-0.0.1.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/Math/XS.pm
@@ -0,0 +1,21 @@
+use strict; use warnings;
+package Acme::Math::XS;
+# our $VERSION = '0.0.1';
+
+use Exporter 'import';
+our @EXPORT = qw(
+    add
+    subtract
+);
+
+use Acme::Math::XS::Inline CPP => <<'...';
+long add(long a, long b) {
+    return a + b;
+}
+
+long subtract(long a, long b) {
+    return a - b;
+}
+...
+
+1;
